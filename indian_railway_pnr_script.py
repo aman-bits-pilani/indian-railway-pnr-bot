b373b7c034b8479010fd06627461c21e8ca01dd9
Aman Kumar Gupta <202217B3005@wilp.bits-pilani.ac.in>
2025-05-12 17:03:10 +0530

    Update indian_railway_pnr_script.py

--- indian_railway_pnr_script.py
+++ indian_railway_pnr_script.py
@@ -45,6 +45,6 @@ def fetch_pnr_status(pnr):
         driver.quit()
 
 # Example usage
-pnrs = ["2135012418", "2663243998", "2715704719"]
+pnrs = ["2135012418", "2715707015", "2663243998", "2715704719"]
 for pnr in pnrs:
     fetch_pnr_status(pnr)
